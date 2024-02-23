43d581805518c0ddf7d2cb97c5a0803c53e40370
snubydev <petelin78@gmail.com>
2024-02-23 13:35:08 +0100

    add help, args to main, update readme

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
 # ![(avatar)](https://avatars.githubusercontent.com/u/27973237?s=48&v=4)   Zig parser example
 
-Parse a simple expression with binary oprators and calculate result.
+Parse a simple arithmetic expression with binary oprators and calculate result.
 
 ### Example    
-given a string with math expression "500 * 13 + 8 * 10"    
+given a string with math expression "500 * ( 13 + 8 ) * 10"    
 parse and calculate the result?
 
-### Run
+### Usage
 Install zig 0.11+ programming language from [ziglang.org](https://ziglang.org/learn/getting-started/#installing-zig)
 ```
-$ zig test main.zig
+$ zig build-exe main.zig
+
+$ ./main "500 * ( 13 + 8 ) * 10"
+
+```
+
+### Tests
+```
+$ zig test test_main.zig
+
+$ zig test test_parser.zig
 ```
 ### Links 
 Inspired by Jonathan Blow ["Discussion with Casey Muratori about how easy precedence is..."](https://www.youtube.com/watch?v=fIPO4G42wYE) (youtube)
--- main.zig
+++ main.zig
@@ -29,16 +29,39 @@ pub fn calculate(node: *Node) i32 {
     return 0;
 }
 
+fn help() void {
+    std.debug.print("Usage:\n", .{});
+    std.debug.print("   calc \"<expression>\"\n\n", .{});
+    std.debug.print("      - expression: variables, integer numbers, operators + - * /, brackets ( )\n", .{});
+    std.debug.print("      - IMPORTANT: all tokens in the expression should be separated by <space>\n\n", .{});
+    std.debug.print("Example:\n", .{});
+    std.debug.print("   calc \"( 5 + 3 ) * 7\"\n\n", .{});
+    std.debug.print("Help:\n", .{});
+    std.debug.print("   calc --help\n", .{});
+}
+
 pub fn main() !void {
     std.debug.print("Hello from zig-parser!\n", .{});
 
+    // read args
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    if (args.len != 2) {
+        help();
+        return;
+    }
+
     var operators = [_][]const u8{ "+", "-", "*", "/" };
-    const text = "500 + 3 * 8 * ( ( 10 + 3 ) - ( 5 + 4 * 4 ) )";
-    print("[text] {s}\n", .{text});
+    const text = args[1];
+    print("[input] {s}\n", .{text});
     var p = try Parser.init(std.heap.page_allocator, text, &operators, null);
     const head = try p.parseExpression(0);
     defer p.deinit(head);
     const result = calculate(head);
     print("[result] {d}\n", .{result});
-    //try testing.expectEqual(764, result);
 }
